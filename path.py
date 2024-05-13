39cf9fca9f57ddf7ed8e4061b7c83320fc15d8ad
Jiramethong <ongardanunkul.j@gmail.com>
2024-05-13 08:32:05 +0700

    hide_path

--- path.py
+++ path.py
@@ -1,6 +1,6 @@
-path_pdt_params = r"C:\Users\ssgro\OneDrive\Documents\CleanData\1.PDT_parameter.csv"
-path_qc_product = r"C:\Users\ssgro\OneDrive\Documents\CleanData\2.PDT_QC_product.csv"
-path_feed_speed = r"C:\Users\ssgro\OneDrive\Documents\CleanData\3.FeedRawMaterial_Speed.csv"
-path_feed_material = r"C:\Users\ssgro\OneDrive\Documents\CleanData\4.FeedRawMaterial_Quality_Data.csv"
-path_pdt_form = r"C:\Users\ssgro\OneDrive\Documents\CleanData\5.Production cleanOnlyPDT.csv"
+path_pdt_params = r".\1.PDT_parameter.csv"
+path_qc_product = r".\2.PDT_QC_product.csv"
+path_feed_speed = r".\3.FeedRawMaterial_Speed.csv"
+path_feed_material = r".\4.FeedRawMaterial_Quality_Data.csv"
+path_pdt_form = r".\5.Production cleanOnlyPDT.csv"
 
